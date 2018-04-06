52e1f37ed58ca1dcd8ca0a953dc40fa88aafe60b
ajking98 <agedi98@gmail.com>
2018-04-06 15:19:05 -0400

    second commit

---
+++ Challenge 1/Question 1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int i;
+    long lo;
+    char ch;
+    float fl;
+    double d;
+    scanf("%d %ld %c %f %lf" , &i, &lo, &ch, &fl, &d);
+    printf("%d\n%ld\n%c\n%f\n%lf" , i, lo, ch, fl, d);
+    return 0;
+}
