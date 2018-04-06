1e230097fac29dc437b1ec2fd9230dbe7a46d801
ajking98 <agedi98@gmail.com>
2018-04-06 15:22:03 -0400

    second commit

---
+++ Challenge 1/Question 2/main.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int first;
+    int second;
+    int third;
+    int result;
+    cin >> first >> second >> third;
+    result = first + second + third;
+    cout << result << endl;
+    return 0;
+}
